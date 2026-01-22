c7e8eff61225aee92e3bc51bd7bd11d386eeff4c
ngocnhtl00231-commits <ngocnhtl00231@gmail.com>
2026-01-22 13:33:08 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Nike Air Force 1",
+    price: 2500000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/air-force-1.jpg"
+  },
+  {
+    id: 2,
+    name: "Nike Air Jordan 1",
+    price: 4200000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/air-jordan-1.jpg"
+  },
+  {
+    id: 3,
+    name: "Nike Air Max 97",
+    price: 3800000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/air-max-97.jpg"
+  }
+];
